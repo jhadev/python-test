3bc4784552684403b75f5e06e35be7c599a14390
JA <joshappeldev@gmail.com>
2018-12-06 12:10:58 -0500

    just messing around

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 print("python")
 print("python " * 10)
 
-x = 1
-y = 2
+x, y = 1, 2
 z = 16
 unit_price = 3
 
 print(unit_price * y * z)
+
+students_count = 1000
+print(type(students_count))
+
+rating = 4.99
+print(type(rating))
+
+is_published = False
+print(type(is_published))
+
+course_name = "Python"
+print(type(course_name))
